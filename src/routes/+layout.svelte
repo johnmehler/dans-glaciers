cd7458285e0da9419ad30c969035bfc0eeb3354c
John Mehler <johnmehler@gmail.com>
2026-03-07 19:32:31 -0500

    favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import "../app.css";
-	import favicon from "$lib/assets/favicon.svg";
+	import favicon from "$lib/assets/fav.png";
 	import Header from "$lib/Header.svelte";
 	import Footer from "$lib/Footer.svelte";
 </script>
